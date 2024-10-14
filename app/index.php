84acf0fa3e73c4eeecb0a209f5057ea71e25606d
mirobili <mirobili@mail.bg>
2024-10-15 01:57:50 +0300

    Add files via upload

--- app/index.php
+++ app/index.php
@@ -3,13 +3,7 @@ declare(strict_types=1);
 
 namespace App;
 
-//const ROOT_DIR = 'C:\www\Credissimo\mvc';
-
-
 
 require_once './vendor/autoload.php';
 
 require_once './src/Routes/routes.php';
-
-
-trace(getenv());
